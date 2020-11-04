9f6efd989ea8c10ea968331222c7ad6d6705f5a8
Daniel M. Lofaro <daniel.lofaro@nrl.navy.mil>
2020-11-04 00:48:40 -0500

    joint contorl with effor workign

--- gazebo/differential_drive_robot/src/ctrlJoint.py
+++ gazebo/differential_drive_robot/src/ctrlJoint.py
@@ -9,20 +9,28 @@ def talker():
     rospy.init_node('dd_ctrl', anonymous=True)
     pub = rospy.ServiceProxy('/gazebo/apply_joint_effort',ApplyJointEffort)
     rate = rospy.Rate(10) # 10hz
+    i = 0
+    buff = ApplyJointEffort()
+    buff.joint_name = "dd_robot::left_wheel_hinge"
+    buff.effort = 1.0
+    buff.start_time = 0.0
+    buff.duration = 0.0
+    start_time = rospy.Time(0,0)
+    end_time = rospy.Time(0.01,0)
     while not rospy.is_shutdown():
-        buff = ApplyJointEffort()
-        buff.joint_name = "dd_robot::left_wheel_hinge"
-        buff.effort = 0.0
-        buff.start_time = 0.0
-        buff.duration = 0.0
-        start_time = rospy.Time(0,0)
-        end_time = rospy.Time(0.1,0)
+        if i > 30:
+            buff.effort = -buff.effort
+            i = 0
+        i = i + 1
 
 
         # ['joint_name', 'effort', 'start_time', 'duration']
         #pub( buff.joint_name, buff.effort, buff.start_time, buff.duration )
         #pub( buff.joint_name, buff.effort, 0.0, 0.1 )
-        pub(buff.joint_name, 0.1, start_time, end_time)
+        buff.joint_name = "dd_robot::left_wheel_hinge"
+        pub(buff.joint_name, buff.effort, start_time, end_time)
+        buff.joint_name = "dd_robot::right_wheel_hinge"
+        pub(buff.joint_name, buff.effort, start_time, end_time)
         rospy.loginfo(buff)
         rate.sleep()
 
