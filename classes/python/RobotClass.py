fc212a34e51f5140f43a926c7fb8e999a82d712a
Daniel M. Lofaro <daniel.lofaro@nrl.navy.mil>
2021-07-07 10:35:12 -0400

    added example python class

---
+++ classes/python/RobotClass.py
@@ -0,0 +1,5 @@
+class RobotClass:
+    the_var = None
+    def __init__(self, var):
+        self.the_var = var
+        return
---
+++ classes/python/robot.py
@@ -0,0 +1,7 @@
+from RobotClass import RobotClass as RC
+
+rc1 = RC(42)
+rc2 = RC(57)
+
+print(rc1.the_var)
+print(rc2.the_var)
