494f680ac04111dbcb25f31fa796f23457ba3761
Daniel M. Lofaro <daniel.lofaro@nrl.navy.mil>
2021-07-07 10:50:36 -0400

    added examples of modifying variables

--- classes/python/RobotClass.py
+++ classes/python/RobotClass.py
@@ -1,5 +1,31 @@
 class RobotClass:
     the_var = None
+    the_global = None
+    the_self = None
+
     def __init__(self, var):
+
+        # Changes instance of the_var, viewable by rest of class
         self.the_var = var
+
+        # local version of the_var not seen by the rest of class
+        the_var = var * 2
+
+        return
+
+    def setGlobal(self, var):
+        global the_global
+        return
+
+    def setSelf(self, var):
+        self.the_self = var
+        return
+
+    def getGlobal(self):
+
+        # The statement below does NOT work as the_global is not defined
+        # print(the_global)
+
+        # This statement does work as it is an instance of classes "the_global"
+        print(self.the_global)
         return
--- classes/python/robot.py
+++ classes/python/robot.py
@@ -5,3 +5,32 @@ rc2 = RC(57)
 
 print(rc1.the_var)
 print(rc2.the_var)
+
+print()
+
+# Global changes are not viewable from outside the class
+rc1.setGlobal(123)
+rc2.setGlobal(-123)
+print(rc1.the_global)
+print(rc2.the_global)
+print(rc1.the_global)
+print()
+
+# We can set global values manually
+rc1.the_global=123
+rc2.the_global=-123
+print(rc1.the_global)
+print(rc2.the_global)
+print(rc1.the_global)
+rc1.getGlobal()
+rc2.getGlobal()
+print()
+
+
+# Self changes are viewable outside the class
+rc1.setSelf(3.14)
+rc2.setSelf(-3.14)
+print(rc1.the_self)
+print(rc2.the_self)
+print(rc1.the_self)
+print()
